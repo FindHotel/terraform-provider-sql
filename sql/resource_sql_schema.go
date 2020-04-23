2d1cfd64966ab013f61ad18b413604e0e1370476
Andrew Kumanyaev <me@zzet.org>
2020-04-23 18:42:23 +0400

    Fix the problem with the migrations list

--- sql/resource_sql_schema.go
+++ sql/resource_sql_schema.go
@@ -69,7 +69,7 @@ func resourceSQLSchema() *schema.Resource {
 				Description: "Name of the table to use to store applied migrations.",
 			},
 			"migrations": &schema.Schema{
-				Type:     schema.TypeSet,
+				Type:     schema.TypeList,
 				Computed: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
